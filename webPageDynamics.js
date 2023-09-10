95c1f4ff180e634e24238f92064941ef7da3bbcd
William <williamnorland@gmail.com>
2023-09-10 23:49:13 +0200

    css and js work

--- webPageDynamics.js
+++ webPageDynamics.js
@@ -1,5 +1,4 @@
 
-
 function typeText(id, text, delay, start) {
 
     let element = document.getElementById(id);
@@ -12,4 +11,18 @@ function typeText(id, text, delay, start) {
         setTimeout(typeText, delay, id, text, delay, start+1);
     }
 
+}
+
+function blinkingUnderScoreAfter(id, delay) {
+    let element = document.getElementById(id);
+    let lastIndex = element.textContent.length - 1
+
+    if (element.textContent.charAt(lastIndex) != "_") {
+        element.textContent += "_"
+        setTimeout(blinkingUnderScoreAfter, delay, id, delay)
+    } 
+    else {
+        element.textContent = element.textContent.slice(0, -1);
+        setTimeout(blinkingUnderScoreAfter, delay, id, delay)
+    }
 }
