a992eda88c748933ab5b90330e8cd723183ebaea
William <williamnorland@gmail.com>
2023-10-26 22:44:04 +0200

    removed comment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_webpage():
-    return render_template('mainPage.html')  # Replace 'your_html_file.html' with the actual filename.
+    return render_template('mainPage.html')
 
 if __name__ == '__main__':
     serve(app, host="127.0.0.1", port=5000)
